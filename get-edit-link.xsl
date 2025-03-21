42c3e05aa7580f520848b0f2ad81a026564a2509
Clinton Graham <ctgraham@pitt.edu>
2025-03-21 10:56:06 -0400

    Test deposit, fetch, update

---
+++ get-edit-link.xsl
@@ -0,0 +1,8 @@
+<xsl:stylesheet version="1.0" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:output method="text" />
+	<xsl:template match="/">
+		<xsl:for-each select="/atom:feed/atom:link[@rel='edit']">
+			<xsl:value-of select="@href" /><xsl:text>&#10;</xsl:text>
+		</xsl:for-each>
+	</xsl:template>
+</xsl:stylesheet>
--- test.sh
+++ test.sh
@@ -1,4 +1,68 @@
 #!/bin/bash
+
+# Request a SWORD URI ($1), put results into temp file ($2), via HTTP Method ($3)
+function sword_request {
+	echo ${3} $SWORD_ENDPOINT/$1 into $2
+	curl $SWORD_CURL_OPTS -s -D $SWORD_OUTPUT/$2.headers -o $SWORD_OUTPUT/$2.out \
+		--request $3 \
+		--url $SWORD_ENDPOINT/$1 \
+		--header 'Content-Type: application/xml' \
+		$SWORD_CURL_AUTH_KEY "$SWORD_CURL_AUTH_VAL" \
+		$SWORD_POST_HEADER_KEY1 "$SWORD_POST_HEADER_VAL1" \
+		$SWORD_POST_HEADER_KEY2 "$SWORD_POST_HEADER_VAL2" \
+		$SWORD_POST_HEADER_KEY2 "$SWORD_POST_HEADER_VAL2" \
+		$SWORD_POST_FILE_KEY "$SWORD_POST_FILE_VAL" 
+	SWORD_HTTP_CODE=`head -1 $SWORD_OUTPUT/service_document.headers`
+	if [ `echo $SWORD_HTTP_CODE | cut -d' ' -f2 | cut -c 1` != "2" ]
+	then
+		>&2 echo ... returned $SWORD_HTTP_CODE
+		exit 2
+	fi
+	xmllint --noout --nowarning $SWORD_OUTPUT/$2.out
+	if [ $? -ne 0 ]
+	then
+		>&2 echo ... $2 not valid
+		exit 2
+	fi
+}
+
+function sword_get {
+	SWORD_POST_FILE_KEY=
+	SWORD_POST_FILE_VAL=
+	SWORD_POST_HEADER_KEY1=
+	SWORD_POST_HEADER_KEY2=
+	SWORD_POST_HEADER_KEY3=
+	SWORD_POST_HEADER_VAL1=
+	SWORD_POST_HEADER_VAL2=
+	SWORD_POST_HEADER_VAL3=
+	sword_request $1 $2 GET
+}
+
+function sword_post {
+	SWORD_POST_FILE_KEY=$3
+	SWORD_POST_FILE_VAL=$4
+	SWORD_POST_HEADER_KEY1=
+	SWORD_POST_HEADER_KEY2=
+	SWORD_POST_HEADER_KEY3=
+	if [ "$5" != "" ]
+	then
+		SWORD_POST_HEADER_KEY1='--header'
+	fi
+	if [ "$6" != "" ]
+	then
+		SWORD_POST_HEADER_KEY2='--header'
+	fi
+	if [ "$7" != "" ]
+	then
+		SWORD_POST_HEADER_KEY3='--header'
+	fi
+	SWORD_POST_HEADER_VAL1=$5
+	SWORD_POST_HEADER_VAL2=$6
+	SWORD_POST_HEADER_VAL3=$7
+	sword_request $1 $2 POST
+}
+
+# Sanity check the confug
 if [ -e .env ]
 then
 	source .env
@@ -8,31 +72,24 @@ then
 	>&2 echo SWORD_ENDPOINT is undefined
 	exit 1
 fi
+SWORD_CURL_AUTH_KEY=
+SWORD_CURL_AUTH_VAL=
 if [ "$SWORD_APIKEY" != "" ]
 then
 	SWORD_CURL_AUTH_KEY="--header"
 	SWORD_CURL_AUTH_VAL="Api-key: $SWORD_APIKEY"
 fi
 SWORD_OUTPUT=`mktemp -d output/sword.XXXXX`
+if [ "$SWORD_OUTPUT" = "" -o ! -d $SWORD_OUTPUT ]
+then
+	>&2 echo Failed to create temporary directory
+        exit 1
+fi
+
+# Sanity check the Service
 echo Writing tests to $SWORD_OUTPUT
-echo Fetching service document
-curl $SWORD_CURL_OPTS -D $SWORD_OUTPUT/service_document.headers -o $SWORD_OUTPUT/service_document.out \
-	--request GET \
-	--url $SWORD_ENDPOINT/service_document \
-	--header 'Content-Type: application/xml' \
-	$SWORD_CURL_AUTH_KEY "$SWORD_CURL_AUTH_VAL"
-SWORD_HTTP_CODE=`head -1 $SWORD_OUTPUT/service_document.headers`
-if [ `echo $SWORD_HTTP_CODE | cut -d' ' -f2 | cut -c 1` != "2" ]
-then
-	>&2 echo ... returned $SWORD_HTTP_CODE
-	exit 2
-fi
-xmllint --noout --nowarning $SWORD_OUTPUT/service_document.out
-if [ $? -ne 0 ]
-then
-	>&2 echo ... Service Document not valid
-	exit 2
-fi
+SWORD_OUTFILE=service_document
+sword_get service_document $SWORD_OUTFILE
 if [ "$SWORD_COLLECTIONS_XSL" = "" -a "$SWORD_COLLECTION" = "" ]
 then
 	SWORD_COLLECTIONS_XSL="get-all-collections.xsl"
@@ -44,12 +101,60 @@ then
 		>&2 echo ... Transform not found
 		exit 3
 	fi
-	SWORD_COLLECTIONS=`xsltproc $SWORD_COLLECTIONS_XSL $SWORD_OUTPUT/service_document.out`
+	SWORD_COLLECTIONS=`xsltproc $SWORD_COLLECTIONS_XSL $SWORD_OUTPUT/$SWORD_OUTFILE.out`
 fi
 if [ "$SWORD_COLLECTION" != "" ]
 then
 	SWORD_COLLECTIONS=$SWORD_COLLECTION
 fi
-echo I will process: $SWORD_COLLECTIONS
+for SWORD_CURRENT in $SWORD_COLLECTIONS
+do
+	SWORD_OUTFILE=collection
+	sword_get collections/$SWORD_CURRENT $SWORD_OUTFILE
+done
+
+# Try deposits
+for SWORD_CURRENT in $SWORD_COLLECTIONS
+do
+	for m in input/*/metadata.xml
+	do
+		# Try metadata deposit
+		SWORD_OUTFILE=metadata-only
+		sword_post collections/$SWORD_CURRENT/works/ $SWORD_OUTFILE \
+			'--data-binary' "@${m}" \
+			'Content-Disposition: attachment; filename=metadata.xml' \
+			'Packaging: application/atom+xml;type=entry'
+		# What URI was created for this work?
+		SWORD_URI=`xsltproc get-src-link.xsl $SWORD_OUTPUT/$SWORD_OUTFILE.out | sed "s|$SWORD_ENDPOINT/||"`
+		if [ "$SWORD_URI" = "" ]
+		then
+			>&2 echo ... Resulting link src not found
+			exit 4
+		fi
+		# Try get work
+		SWORD_OUTFILE=metadata-returned
+		sword_get $SWORD_URI $SWORD_OUTFILE
+		# What URI was created for the files?
+		SWORD_URI=`xsltproc get-edit-link.xsl $SWORD_OUTPUT/$SWORD_OUTFILE.out | sed "s|$SWORD_ENDPOINT/||"`
+		if [ "$SWORD_URI" = "" ]
+		then
+			>&2 echo ... Resulting link edit not found
+			exit 4
+		fi
+		# Try file deposit
+		for f in `dirname $m`/files/*
+		do
+			SWORD_OUTPUT=file-deposit
+			sword_post $SWORD_URI $SWORD_OUTFILE \
+			'--data-binary' "@${f}" \
+			'Packaging: http://purl.org/net/sword/package/Binary' \
+			'Content-Disposition: attachment; filename='`basename $f` \
+			'Content-type: '`file --brief --mime $f | cut -d';' -f1`
+		done
+		# Try updates
+	done
+done
+
+# Success, cleanup
 rm -fr $SWORD_OUTPUT
 
